6b65288d14e3fd919598ea23e6335ee6b95a6a44
AnastasiiaLarina <laranweb@gmail.com>
2024-03-16 19:34:18 +0500

    Initial Commit

---
+++ CryptyCo/CryptyCoApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptyCoApp.swift
+//  CryptyCo
+//
+//  Created by Анастасия Ларина on 16.03.2024.
+//
+
+import SwiftUI
+
+@main
+struct CryptyCoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
